34b75057f281ed6a3c4a5caef5a64281fdaecc93
OrhanMA <orhan.madi.assani@gmail.com>
2023-12-31 23:31:28 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini message board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New message" });
+});
+
+router.post("/new", function (req, res, next) {
+  const author = req.body.author;
+  const message = req.body.message;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
+});
+module.exports = router;
